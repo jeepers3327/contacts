bc432da99fd86e73616da81475551722b752f334
Jhefrey Sajot <heifurukawa1997@gmail.com>
2021-06-27 23:25:02 +0800

    modify Dockerfile for dev build

---
+++ api/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Docker entrypoint script.
+
+# Wait until Postgres is ready
+
+while ! pg_isready -q  -h $DB_HOST -p 5432 -U $DB_USER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+/app/bin/api eval Api.Release.migrate
+/app/bin/api start
+
+
